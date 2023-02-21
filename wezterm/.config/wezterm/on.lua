c4dd253a114ed0d648167e0bda1163dfe7d5a1cb
Jeffrey <double12gzh@gmail.com>
2023-02-22 01:30:45 +0800

    add battery and date on entering wezterm

--- wezterm/.config/wezterm/on.lua
+++ wezterm/.config/wezterm/on.lua
@@ -17,3 +17,17 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 
 	return { { Text = title } }
 end)
+
+wezterm.on('update-right-status', function(window, pane)
+        -- "Wed Mar 3 08:14"
+        local date = wezterm.strftime '%a %b %-d %H:%M '
+
+        local bat = ''
+        for _, b in ipairs(wezterm.battery_info()) do
+          bat = '🔋 ' .. string.format('%.0f%%', b.state_of_charge * 100)
+        end
+
+        window:set_right_status(wezterm.format {
+          { Text = bat .. '   ' .. date },
+        })
+end)
