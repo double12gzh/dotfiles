bafdcd14e09b11cef1c053d018470c146d713e0d
Jeffrey <double12gzh@gmail.com>
2024-08-30 00:27:20 +0800

    Update .local_aliases.zsh

--- zsh/.zsh/.local_aliases.zsh
+++ zsh/.zsh/.local_aliases.zsh
@@ -12,19 +12,32 @@ alias k8s='server_me="$(\cat $HOME/.zsh/sshs/hosts/k8s | fzf)" && $HOME/.auto_lo
 ##  1.2.3.5
 
 alias example='mycolorfulssh example'
-
-proxy="http://127.0.0.1:7890"
-noproxy="127.0.0.1,localhost,.gitlab.centos.cn"
-
-proxy_ip="172.21.22.180"
-proxy_port="7897"
+DEFAULT_NOPROXY=(
+    "127.0.0.1"
+    "localhost"
+    "*.local"
+    "*.srdcloud.cn"
+    "*.ctyun.cn"
+    "30.8/16"
+    "21.57/16"
+    "150.223/16"
+    "169.254/16"
+    "10.113/16"
+)
+DEFAULT_PROXY_IP=192.168.175.24
+DEFAULT_PROXY_PORT=7897
 
 __setup_system_proxies() {
+    local proxy_ip="${1:-$DEFAULT_PROXY_IP}"
+    local proxy_port="${2:-$DEFAULT_PROXY_PORT}"
+
     case "$(uname -s)" in
     Darwin)
         networksetup -setwebproxy "Wi-Fi" ${proxy_ip} ${proxy_port}
         networksetup -setsecurewebproxy "Wi-Fi" ${proxy_ip} ${proxy_port}
         networksetup -setsocksfirewallproxy "Wi-Fi" ${proxy_ip} ${proxy_port}
+
+        networksetup -setproxybypassdomains "Wi-Fi" "${DEFAULT_NOPROXY[@]}"
         ;;
     Linux)   echo "TODO";;
     *)       echo "Unknown";;
@@ -37,29 +50,22 @@ __teardown_system_proxies() {
         networksetup -setwebproxystate "Wi-Fi" off
         networksetup -setsecurewebproxystate "Wi-Fi" off
         networksetup -setsocksfirewallproxystate "Wi-Fi" off
+        networksetup -setproxybypassdomains "Wi-Fi" ""
         ;;
     Linux)  echo "TODO";;
     *)      echo "Unknown";;
     esac
 }
 
-__gansters() {
-    if [ ! -z "$1" ];then
-        # 暂时不上传，择机上传
-        ~/.gangsters/gangsters "$1"
-    else
-        __help
-    fi
-}
-
 __setup_proxies() {
-    # export {http_proxy,https_proxy,all_proxy}=$proxy
+    local proxy_ip="${1:-$DEFAULT_PROXY_IP}"
+    local proxy_port="${2:-$DEFAULT_PROXY_PORT}"
 
-    export http_proxy="$proxy"
-    export https_proxy="$proxy"
-    export all_proxy="$proxy"
+    export http_proxy="http://${proxy_ip}:${proxy_port}"
+    export https_proxy="http://${proxy_ip}:${proxy_port}"
+    export all_proxy="socks5://${proxy_ip}:${proxy_port}"
 
-    export no_proxy="$noproxy"
+    export no_proxy=$(IFS=,; echo "${DEFAULT_NOPROXY}")
 }
 
 __teardown_proxies() {
@@ -75,21 +81,29 @@ __check_proxies() {
     echo "  https_proxy:    $https_proxy"
     echo "  all_proxy:      $all_proxy"
     echo "  no_proxy:       $no_proxy"
-    if [[ `uname -s` == "Darwin" ]];then
+    if [[ uname -s == "Darwin" ]];then
         echo "\n>>>> scutil --proxy <<<<\n"
         scutil --proxy
     fi
 }
 
 __help() {
-    echo "Usage: cmd/cmds [options]"
-    echo "  -p, --proxy     Setup system proxies"
-    echo "  -t, --teardown  Teardown system proxies"
-    echo "  -s, --setup     Setup proxies ENVs"
-    echo "  -d, --destroy   Teardown proxies ENVs"
-    echo "  -c, --check     Check proxies status"
-    echo "  -i, --ip        Check ip"
+    echo "Usage: cmd/cmds [options] [proxy_ip] [proxy_port]"
+    echo "  -p, --proxy [proxy_ip] [proxy_port]     Setup system proxies"
+    echo "  -t, --teardown                          Teardown system proxies"
+    echo "  -s, --setup [proxy_ip] [proxy_port]     Setup proxies ENVs"
+    echo "  -d, --destroy                           Teardown proxies ENVs"
+    echo "  -c, --check                             Check proxies status"
+    echo "  -i, --ip                                Check ip"
     echo "  -g, --gangsters [start|stop|status|aone_status|aone_disable|aone_enable] Kill gangsters process"
+}
+
+__gansters() {
+    if [ ! -z "$1" ];then
+        ~/.gangsters/gangsters "$1"
+    else
+        __help
+    fi
 }
 
 __ip() {
@@ -109,16 +123,20 @@ cmd() {
     while [[ "$#" -gt 0 ]]; do
         case "$1" in
             -p|--proxy)
-                __setup_system_proxies
+                shift
+                __setup_system_proxies $1 $2
                 __check_proxies
+                break
                 ;;
             -t|--teardown)
                 __teardown_system_proxies
                 __check_proxies
                 ;;
             -s|--setup)
-                __setup_proxies
+                shift
+                __setup_proxies $1 $2
                 __check_proxies
+                break
                 ;;
             -c|--check)
                 __check_proxies
